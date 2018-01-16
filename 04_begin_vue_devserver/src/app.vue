d875079398c13c1ad2b328978193139d421f944f
XiezhDev <1245356128@qq.com>
2018-01-17 00:59:35 +0800

    随意玩玩，旧玩意，新玩法

--- 04_begin_vue_devserver/src/app.vue
+++ 04_begin_vue_devserver/src/app.vue
@@ -1,12 +1,35 @@
 <template>
     <div>
-        <h1>Hello Vue</h1>
+        <h1>{{text}}</h1>
+        <br>
+        {{text}}
+        <input type="text" v-model="text"/>
+        <hr>
+        <ul>
+            <li v-for="person in persons">
+                {{person.name}}
+            </li>
+        </ul>
     </div>
     <!-- 2.x以后，只能有一个跟节点 -->
 </template>
 
 <script>
-
+    export default {
+        data() {
+            return {
+                text: 'Hello Vue',
+                persons: [
+                    {
+                        name: '谢智辉',
+                    },
+                    {
+                        name: '辉哥'
+                    }
+                ]
+            }
+        }
+    }
 </script>
 
 <style>
