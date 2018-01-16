a25010286e03ddbad3d928642d868366d348c443
XiezhDev <1245356128@qq.com>
2018-01-17 00:48:14 +0800

    初始化项目

---
+++ 04_begin_vue_devserver/README.md
@@ -0,0 +1,2 @@
+使用dev-server启动Vue项目。
+为了简化启动命令，使用临时的package.json文件，定义启动scripts。
---
+++ 04_begin_vue_devserver/src/app.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Hello Vue</h1>
+    </div>
+    <!-- 2.x以后，只能有一个跟节点 -->
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
