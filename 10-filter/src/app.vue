63220ee4c7e8bf22614d308681edc98c881d9430
XiezhDev <1245356128@qq.com>
2018-01-18 00:02:45 +0800

    自定义过滤器

---
+++ 10-filter/src/app.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        请输入要反转的内容：
+        <input type="text" v-model="text"/>
+        <br>
+        反转结果为：{{text | reverseFilter}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                text: '父已托我帮你办点事'
+            }
+        },
+        filters: {
+            reverseFilter: function (value) {// value就是text
+                // 反转字符串
+                return value.split('').reverse().join('');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
