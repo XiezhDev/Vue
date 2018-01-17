9d0becf3ebfe9ff5035ec205436a0b3bb39889f2
XiezhDev <1245356128@qq.com>
2018-01-17 10:07:20 +0800

    初始化项目

---
+++ 05common-directive/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        v-text:（元素的innerText，不能在双标签中使用）
+        <span v-text="text"></span>
+        <hr>
+        v-html:（元素的innerHTML，不能包含<!--{{}}-->
+        <span v-html="html"></span>
+        <hr>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return{
+                text: '我是v-text文本',
+                html:  `
+                    <ul>
+                        <li>谢</li>
+                        <li>智</li>
+                        <li>辉</li>
+                        <li>{{text}}</li>
+                    </ul>
+                `
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
