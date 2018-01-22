e38b6d1fdb21b5427c8df9d7aadd73a059ceb55a
XiezhDev <1245356128@qq.com>
2018-01-23 00:03:50 +0800

    watch

---
+++ 24_watch/src/components/app.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <button @click="changeVue">改变data</button>
+        <button @click="changePerson">改变persons</button>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                data: 'cba',
+                persons: [
+                    {name: 'shenhua'},
+                    {name: 'huige'}
+                ]
+            }
+        },
+        methods: {
+            changeVue() {
+                this.data = 'abc';
+            },
+            changePerson() {
+                this.persons[0].name = 'xiezh';
+            }
+        },
+        watch: {
+            data: function (newValue, oldValue) {
+                alert('data值由' + oldValue + '变为' + newValue);
+            },
+            // 监视复杂类型，需要深度监视
+            persons: {
+                handler: function (val, oldVal) {
+                    alert('person改变了');
+                },
+                deep: true
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
