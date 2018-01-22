56cbffd791bee8f494076ad6f1f72390b989292e
XiezhDev <1245356128@qq.com>
2018-01-23 00:25:46 +0800

    计算属性

---
+++ 25_computed/src/components/app.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        单价：<input type="text" name="" v-model="price"/>
+        件数：<input type="text" name="" v-model="num"/>
+        折扣：<input type="text" name="" v-model="rate"/>=
+        {{sum.name}}/{{sum.price}}
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                price: 0,
+                num: 0,
+                rate: 0
+            }
+        },
+        methods: {
+        },
+        computed: {
+            sum(){
+                // computed可以监视多个this相关属性值的改变
+                //return this.price * this.num * (this.rate/10);
+
+                return {
+                    name: '古龙水',
+                    price: this.price * this.num * (this.rate/10)
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
