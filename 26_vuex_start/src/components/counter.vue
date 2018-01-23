d237982dde456f4cf82f76e74bfd6f51801c74d9
XiezhDev <1245356128@qq.com>
2018-01-23 15:07:12 +0800

    vuex开始项目，模块化版

---
+++ 26_vuex_start/src/components/counter.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <p>{{ count }}</p>
+        <p>
+            <button @click="increment">+</button>
+            <button @click="decrement">-</button>
+        </p>
+    </div>
+</template>
+<script>
+    import store from '../store';
+    export default {
+        data() {
+            return {
+            }
+        },
+        computed: {
+            count () {
+                return store.state.count
+            }
+        },
+        methods: {
+            increment () {
+                store.commit('increment')
+            },
+            decrement () {
+                store.commit('decrement')
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ 26_vuex_start/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import App from './components/counter.vue';
+
+
+
+
+new Vue({
+    el: '#app',
+    render: c => c(App)
+})
